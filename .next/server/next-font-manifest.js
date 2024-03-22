73d0e03a12166b61f2f67645f137263e8af1bc65
Rohit Bommisetti <rohitbommisetti3@gmail.com>
2024-03-22 13:02:35 -0400

    a

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/app/page\": [\n      \"static/media/Geist_Variable-s.p.be1feab1.woff2\",\n      \"static/media/GeistMono_Variable.p.cea06542.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VF/mihdD62F7kVo+wf2gGs0pzUKtZW3C+x14GKRdkwk=\"\n}"
