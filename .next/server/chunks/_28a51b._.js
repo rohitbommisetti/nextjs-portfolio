c178567cfe377bad4640118b11f33555f61e6dba
Rohit Bommisetti <rohitbommisetti3@gmail.com>
2024-03-22 23:20:40 -0400

    fav

--- .next/server/chunks/_28a51b._.js
+++ .next/server/chunks/_28a51b._.js
@@ -489,7 +489,7 @@ __turbopack_esm__({
 })()),
 "[project]/app/favicon.ico [app-rsc] (static)": (({ r: __turbopack_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_dynamic__, p: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, g: global, __dirname }) => (() => {
 
-__turbopack_export_value__("/_next/static/media/favicon.485dedba.ico");
+__turbopack_export_value__("/_next/static/media/favicon.736af45f.ico");
 })()),
 "[project]/app/favicon.ico.mjs/(IMAGE)/[project]/app/favicon.ico [app-rsc] (static) (structured image object, ecmascript)": (({ r: __turbopack_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_dynamic__, p: __turbopack_resolve_absolute_path__, U: __turbopack_relative_url__, R: __turbopack_resolve_module_id_path__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__ }) => (() => {
 "use strict";
@@ -502,8 +502,8 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$favicon$2e$ico__$5b$a
 ;
 const __TURBOPACK__default__export__ = {
     src: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$favicon$2e$ico__$5b$app$2d$rsc$5d$__$28$static$29$__["default"],
-    width: 32,
-    height: 32
+    width: 48,
+    height: 48
 };
 
 })()),
